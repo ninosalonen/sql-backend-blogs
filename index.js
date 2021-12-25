86f7efdf84cd279022487bb55db89c60b7be7595
ninosalonen <ninosalonen@gmail.com>
2021-12-25 12:59:58 +0200

    jou

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 require('express-async-errors')
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
-const { Query } = require('pg/lib/client')
 const app = express()
+
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -14,15 +15,59 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+)
+
 app.get('/', async (req, res) => {
   res.send('Welcome')
 })
 
-app.get('/api', async (req, res) => {
-  const notes = await sequelize.query('select * from notes;', {
-    type: QueryTypes.SELECT,
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  const blog = await Blog.create(req.body)
+  res.json(blog)
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const deleted = await Blog.destroy({
+    where: {
+      id: req.params.id,
+    },
   })
-  res.send(notes)
+  res.json(deleted)
 })
 
 const PORT = process.env.PORT || 3001
